1f8b1ab39cb7073eec6ff55651680524a08c295c
w43312im <insiya.mullamitha@student.manchester.ac.uk>
2024-01-01 18:48:16 +0000

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import NavBar from './components/NavBar'
-import HeroSection from './components/HeroSection'
+import NavBar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <NavBar />
-      <div className="container main" >
+      <div className="container main">
         <HeroSection />
-      </div >
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+export default function Footer() {
+  return (
+    <div className="footer container">
+      <div className="footer-section">
+        <p className="title">Hab-It All</p>
+        <p>
+          Hab-It All is a place where you can track your habits and learn to
+          lead a more balanced lifestyle. This service is completely free.
+        </p>
+        <p>&copy;2023 | All Rights Reserved</p>
+      </div>
+      <div className="footer-section">
+        <p className="title">Contact Us</p>
+        <p>habitall@gmail.com</p>
+        <p>123-456-7890</p>
+        <p>13 Cornelia Street UK</p>
+      </div>
+      <div className="footer-section">
+        <p className="title">Socials</p>
+        <p>Instagram</p>
+        <p>Facebook</p>
+        <p>Twitter</p>
+      </div>
+    </div>
+  );
+}
