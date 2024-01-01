bee5eb260d993e20eca9c70e96403d3a8d2a85e2
w43312im <insiya.mullamitha@student.manchester.ac.uk>
2024-01-01 17:51:31 +0000

    Responsive font size

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -14,22 +14,23 @@ export default function HeroSection() {
         </div>
         <div className="col">
           <p className="info-header">&#128640;&nbsp;Chart Your Progress</p>
-          <p>
+          <p className="info">
             Easily track and visualise your habits, turning daily routines into
             stepping stones towards your goals.
           </p>
           <p className="info-header">&#127919;&nbsp;Customised Guidance</p>
-          <p>
+          <p className="info">
             Receive personalised insights and recommendations, helping you
             optimise your path to success.
           </p>
           <p className="info-header">&#128274;&nbsp;Privacy and Security</p>
-          <p>
+          <p className="info">
             Your data is sacred. Rest assured, our platform prioritizes your
             privacy, ensuring a secure space for your personal growth.
           </p>
         </div>
       </div>
+      <button className="btn">Start Your Journey Now</button>
     </>
   );
 }
